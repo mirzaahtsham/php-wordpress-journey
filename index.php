a64a5fa10cc958c01233dd19337bf5767a77a687
Mirza Ahtsham <mirza.ahtsham786@gmail.com>
2026-05-01 16:25:50 +0500

    Day-1 - PHP Basics Variables - Data Types

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+echo "Hello Mirza Ahtsham, Welcome to PHP Learning!";
+?>
+
+<?php
+$name= "Mirza Ahtsham";
+$age= 32;
+
+echo "My name is " . $name . " and I am " . $age . " Years Old";
+
+?>
+
+<?php
+
+$name = "Ahtsham";
+$designation = "Website Designer and Developer";
+$experience = 8;
+
+echo "My name is " . $name . " . I am " . $designation . " . I have " . $experience . "+ years of Experince.";
+
+?>
